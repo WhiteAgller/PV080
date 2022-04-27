0c795e326b32d404a67e7591d44bd3758372eff3
Jan Straka <xstraka8@fi.muni.cz>
2022-04-27 18:58:51 +0200

    correction

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -24,7 +24,7 @@ class RunBinSh(object):
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
-    exec("import urllib%s as urllib" % version)
+    import "urllib%s as urllib" % version
 
 @app.route('/')
 def index():
